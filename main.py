6a30cabd8b098231d256229478526ac5324f877b
NguyenSangtngh <79956221+NguyenSangtngh@users.noreply.github.com>
2024-06-09 23:19:03 +0700

    Update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ try:
     ser = serial.Serial(port="/dev/ttyUSB0", baudrate=9600)
 except:
     print("Modbus485**","Failed to write data")
-
+MQTT_TOPIC_AI="/innovation/valvecontroller/ai"
 m485 = Modbus485(ser)
 relay1_ON  = [1, 6, 0, 0, 0, 255, 201, 138]
 relay1_OFF = [1, 6, 0, 0, 0, 0, 137, 202]
@@ -21,24 +21,20 @@ relay3_OFF = [3, 6, 0, 0, 0, 0, 136, 40]
 soil_temperature =[10, 3, 0, 6, 0, 1, 101, 112]
 soil_moisture = [10, 3, 0, 21, 0, 1, 148, 181]
 
-# m485.modbus485_send(relay1_ON)
-# time.sleep(2)
-# m485.modbus485_send(relay1_OFF)
-# time.sleep(2)
-# m485.modbus485_send(relay2_ON)
-# time.sleep(2)
-# m485.modbus485_send(relay2_OFF)
-# time.sleep(2)
-# m485.modbus485_send(relay3_ON)
-# time.sleep(2)
-# m485.modbus485_send(relay3_OFF)
-# time.sleep(2)
+mqtt=MQTTHelper()
+# Định nghĩa một hàm xử lý để nhận các thông điệp từ máy chủ MQTT
+def receive_callback(message):
+    print("Received message:", message)
+
+# Gọi phương thức setRecvCallBack để gán hàm xử lý cho việc nhận thông điệp
+mqtt.setRecvCallBack(receive_callback)
 
 while True:
-    # m485.modbus485_send(relay1_ON)
-    # time.sleep(1)
-    # m485.modbus485_read_adc()
-    # time.sleep(5)
+    m485.modbus485_send(relay1_ON)
+    time.sleep(1)
+    m485.modbus485_read_adc()
+    time.sleep(5)
+    mqtt.publish(MQTT_TOPIC_AI,"hello")
     # m485.modbus485_send(relay1_OFF)
     # time.sleep(1)
     # m485.modbus485_read_adc()
@@ -48,8 +44,8 @@ while True:
     # time.sleep(1)
     # m485.modbus485_read_adc()
     # time.sleep(1)
-    m485.modbus485_send(soil_moisture)
-    time.sleep(1)
-    m485.modbus485_read_adc()
-    time.sleep(1)
+    # m485.modbus485_send(soil_moisture)
+    # time.sleep(1)
+    # m485.modbus485_read_adc()
+    # time.sleep(1)
 
